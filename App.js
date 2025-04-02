62bd6cbde05121a139738c8690519095159762f0
Hamidreza Alikhani <hamidrezaalikhani@Hamidrezas-MacBook-Pro.local>
2025-04-02 11:48:07 -0700

    uploaded

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { SafeAreaView } from 'react-native';
+import VoiceRecorder from './components/VoiceRecorder';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+      <VoiceRecorder />
+    </SafeAreaView>
   );
 }
 
